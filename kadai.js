00f1b4d4f1ac6376cf7e5bd5e86b171c09789be9
yosshinsan <yoshiari1220@gmail.com>
2020-10-19 21:03:35 +0900

    update

--- kadai.js
+++ kadai.js
@@ -1,6 +1,8 @@
 $(function(){
 
     $('.yoshi-isClickedIcon').click(function(){      
+        
+        //アイコンの切り替え
         const selectedIconAttr = 'icon: lock; ratio: 2';
         const notselectedIconAttr = 'icon: unlock; ratio: 2'
         
@@ -12,6 +14,15 @@ $(function(){
         }else{
             $(this).attr({'uk-icon':'icon: lock; ratio: 2'});
         }
+
+        //Clickedのカウント
+        let currentClicked = Number($('#clicked_counter').text());
+        currentClicked +=1;
+        $('#clicked_counter').text(currentClicked);
+        //let animationClass = 'uk-animation-shake'
+        //$('#clicked_counter').removeClass(animationClass).addClass(animationClass);
+        $('#clicked_counter').addClass('uk-animation-shake');
+        
     });
 
 });
